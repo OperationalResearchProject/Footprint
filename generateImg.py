720b5923d3fe8d521cafb787edf792582f158183
Pruvost Geoffrey <geoffrey.pruvost@hotmail.com>
2018-02-11 20:21:40 +0100

    add automatic generation

--- generateImg.py
+++ generateImg.py
@@ -1,5 +1,4 @@
 import cv2
-import csv
 import numpy as np
 from numpy import genfromtxt
 
@@ -7,13 +6,15 @@ from numpy import genfromtxt
 def csv_reader(file_obj):
     return genfromtxt(file_obj, delimiter=' ')
 
-def normalize_array(x):
-	from sklearn.preprocessing import MinMaxScaler
-	scaler = MinMaxScaler()
-	scaler.fit(x)
-	return scaler.transform(x)
 
-def main(csv_path,image_name):
+def normalize_array(x):
+    from sklearn.preprocessing import MinMaxScaler
+    scaler = MinMaxScaler()
+    scaler.fit(x)
+    return scaler.transform(x)
+
+
+def generateImage(csv_path, image_name):
     matrice = csv_reader(csv_path)
     # On normalise entre 0 et 1
     nm = normalize_array(matrice)
--- main.py
+++ main.py
@@ -1,13 +1,40 @@
-import QAPGenerators.makeQAPrl
 import subprocess
-import generateImg
+from generateImg import generateImage
+
 
 def main():
-	for i in range(0,5):
-		bashCommand = "random_walk/build/randomWalk QAPGenerators/instance.dat 1 500 footprint.dat"
-		process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-		output, error = process.communicate()
-		generateImg.main("footprint.dat",str(i+6)+".png")
+	instance_size = 10
+	number_instance = 2
+
+	generateInstances(instance_size, number_instance)
+	generateFootprint(instance_size, number_instance)
+
+
+def generateInstances(instance_size, number):
+	bash_command = "QAPGenerators/doInstances.sh " + str(instance_size) + " " + str(number)
+	process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+
+
+def generateFootprint(instance_size, number):
+	binary = "random_walk/build/randomWalk"
+	seed = "1"
+	walk_length = "500"
+
+	for i in range(number):
+		instance_file_rl = "instances/qap_rl_" + str(instance_size) + "_" + str(i) + ".dat"
+		output_file_rl = "random_walk/data/footprint_rl_" + str(instance_size) + "_" + str(i) + ".dat"
+		bash_command = str(binary) + " " + str(instance_file_rl) + " " + str(seed) + " " + str(walk_length) + " " + str(output_file_rl)
+		process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+
+		instance_file_uni = "instances/qap_uni_" + str(instance_size) + "_" + str(i) + ".dat"
+		output_file_uni = "random_walk/data/footprint_uni_" + str(instance_size) + "_" + str(i) + ".dat"
+		bash_command_uni = str(binary) + " " + str(instance_file_uni) + " " + str(seed) + " " + str(walk_length) + " " + str(output_file_uni)
+		print(bash_command_uni)
+		process_uni = subprocess.Popen(bash_command_uni.split(), stdout=subprocess.PIPE)
+
+		generateImage(output_file_uni, "footprints/fp_uni_" + str(instance_size) + "_" + str(i) + ".png")
+		generateImage(output_file_rl, "footprints/fp_rl_" + str(instance_size) + "_" + str(i) + ".png")
+
 
 if __name__ == '__main__':
-    main()
+	main()
