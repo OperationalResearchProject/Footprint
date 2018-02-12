84c7d840b96b7f5148ddf9ef06de929e1c065fa2
Pruvost Geoffrey <geoffrey.pruvost@hotmail.com>
2018-02-12 21:00:10 +0100

    use args and add doc

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import subprocess
+import sys
 from generateImg import generateImage
 
 
 def main():
-	instance_size = 10
-	number_instance = 2
+	if len(sys.argv) < 3:
+		print("Error : ")
+		print(" - first argument : instance size")
+		print(" - second argument : number of instance wanted")
+		exit(-1)
+
+	instance_size = int(sys.argv[1])
+	number_instance = int(sys.argv[2])
 
 	generateInstances(instance_size, number_instance)
 	generateFootprint(instance_size, number_instance)
+
+	print("All footprints pictures are generated in folder footprints/")
 
 
 def generateInstances(instance_size, number):
@@ -31,7 +40,6 @@ def generateFootprint(instance_size, number):
 		instance_file_uni = "instances/qap_uni_" + str(instance_size) + "_" + str(i) + ".dat"
 		output_file_uni = "random_walk/data/footprint_uni_" + str(instance_size) + "_" + str(i) + ".dat"
 		bash_command_uni = str(binary) + " " + str(instance_file_uni) + " " + str(seed) + " " + str(walk_length) + " " + str(output_file_uni)
-		print(bash_command_uni)
 		process_uni = subprocess.Popen(bash_command_uni.split(), stdout=subprocess.PIPE)
 		process_uni.wait()
 
